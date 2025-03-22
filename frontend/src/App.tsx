b0102b28e49f999b9ddd27885609166eb40e458d
milanagurbanova <milana.gurbanova@tum.de>
2025-03-22 20:30:51 +0100

    deleted nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './pages/Login';
 import Upload from './pages/Upload';
-import TopNav from './components/TopNav';
 
 const App: React.FC = () => {
   const navigate = useNavigate();
@@ -13,9 +12,6 @@ const App: React.FC = () => {
 
   return (
     <div className="min-h-screen flex flex-col">
-      {/* Navigation Bar */}
-      <TopNav />
-
       {/* Main Content */}
       <main className="flex-1 bg-cover bg-center bg-no-repeat"
             style={{ backgroundImage: 'url(https://i.pinimg.com/736x/95/c0/7d/95c07d8830438ad513b081f6d5ee848c.jpg)' }}>
